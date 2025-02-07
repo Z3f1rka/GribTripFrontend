f7afd75f77c7d9f7d1af61b0a7b0ba0dcdd13d22
SiveruhinMihail <89450.miha@gmail.com>
2025-02-08 00:49:40 +0400

    Register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,24 +1,73 @@
 <script setup>
 import { FormKit } from '@formkit/vue'
-// This is just a mock of an actual axios instance.s
-const axios = {
-  post: () => new Promise((r) => setTimeout(r, 2000)),
-}
-
-async function login(credentials) {
-  const res = await axios.post(credentials)
-  // do some login things now
-  alert('Logged in!')
+import axios from 'axios'
+import { ref } from 'vue'
+import router from '../router'
+const submitted = ref(false)
+const submitHandler = async (data) => {
+  const res = await new Promise((resolve) => setTimeout(() => resolve(data), 1000))
+  const API = import.meta.env.VITE_API_URL
+  axios.post(API + 'auth/register', res).then((response) => {
+    if (response.data.error) {
+      console.log(response.data.error)
+    } else {
+      localStorage.setItem('token', response.data.access_token)
+      localStorage.setItem('refresh_token', response.data.refresh_token)
+      submitted.value = true
+      router.push('/')
+    }
+  })
+  /* console.log(res) */
 }
 </script>
 <template>
-  <div class="grid grid-cols-2 fixed">
+  <div class="grid grid-cols-2">
     <div class="grid"><img src="/public/image.png" /></div>
     <div class="grid m-20">
-      <h2 class="">Регистрация</h2>
-      <FormKit type="form" submit-label="Login" @submit="login">
-        <FormKit name="email" label="Email address" validation="required|email" />
-        <FormKit type="password" name="password" label="Password" validation="required" />
+      <FormKit
+        type="form"
+        id="registration-example"
+        :form-class="submitted ? 'hide' : 'show'"
+        submit-label="Register"
+        @submit="submitHandler"
+        :actions="false"
+      >
+        <h1 class="text-2xl font-bold mb-12 mt-2">Регистрация!</h1>
+        <div class="mb-5">
+          <FormKit type="text" name="name" label="Имя" placeholder="Миша" validation="required" />
+        </div>
+        <FormKit
+          type="text"
+          name="email"
+          label="Email"
+          placeholder="Misha@example.com"
+          help="Введите почту"
+          validation="required|email"
+        />
+        <div class="double">
+          <FormKit
+            type="password"
+            name="password"
+            label="Пароль"
+            validation="required|length:6|matches:/[^a-zA-Z]/"
+            :validation-messages="{
+              matches: 'Please include at least one symbol',
+            }"
+            placeholder="Пароль"
+            help="Введите пароль"
+          />
+          <FormKit
+            type="password"
+            name="password_confirm"
+            label="Подтвердите пароль"
+            placeholder="Подтвердите пароль"
+            validation="required|confirm"
+            help="Введите пароль"
+          />
+        </div>
+        <div class="mt-5 text-left">
+          <FormKit type="submit">Продолжить ></FormKit>
+        </div>
       </FormKit>
       <div v-if="submitted">
         <h2 class="text-xl text-green-500">Submission successful!</h2>
