de5e29492a2fa09d7380318059782e0113eae622
Z3f1rka <z.e.f.i.r.k.a88877@gmail.com>
2025-02-15 17:11:19 +0400

    Add routing machine

--- src/views/CreateRouteView.vue
+++ src/views/CreateRouteView.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import L from 'leaflet'
+import Routing from ''
 
 const mapContainer = ref()
 const map = ref()
 const userLat = ref(0)
 const userLon = ref(0)
-
 
 onMounted(() => {
   map.value = L.map(mapContainer.value).setView([51.505, -0.09], 13)
@@ -15,10 +15,14 @@ onMounted(() => {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map.value)
 
-  if (navigator.geolocation){
-    navigator.geolocation.watchPosition((position) =>{
-        userLat.value = position.coords.latitude
-        userLon.value = position.coords.longitude
+  L.Routing.control({
+    waypoints: [L.latLng(54.319732, 48.4058184), L.latLng(54.3089, 48.3969)],
+  }).addTo(map.value)
+
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+      userLat.value = position.coords.latitude
+      userLon.value = position.coords.longitude
     })
   }
 })
